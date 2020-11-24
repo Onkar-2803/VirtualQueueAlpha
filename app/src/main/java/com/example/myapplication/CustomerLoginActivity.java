77c8e1cd7472dc2c8d32fdb035c9aa0c8e1d05ae
Parth Patil <parthpatil8399@gmail.com>
2020-11-24 10:24:12 +0530

    New

--- app/src/main/java/com/example/myapplication/CustomerLoginActivity.java
+++ app/src/main/java/com/example/myapplication/CustomerLoginActivity.java
@@ -1,17 +1,63 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CustomerLoginActivity extends AppCompatActivity {
-
+    EditText memail,mpassword;
+    Button mbutton;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_login);
+        memail=findViewById(R.id.customer_email);
+        mpassword=findViewById(R.id.customer_password);
+        mbutton=findViewById(R.id.customer_login);
+        fAuth=FirebaseAuth.getInstance();
+
+        mbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=memail.getText().toString().trim();
+                String password=mpassword.getText().toString().trim();
+                if(TextUtils.isEmpty(email)){
+                    memail.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mpassword.setError("Password is Required");
+                    return;
+                }
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(CustomerLoginActivity.this,"Login Successfully",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),ShopListActivity.class));
+                        }
+                        else {
+                            Toast.makeText(CustomerLoginActivity.this,"Error " +task.getException(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
     public void Login(View view)
     {
--- app/src/main/java/com/example/myapplication/CustomerSignupActivity.java
+++ app/src/main/java/com/example/myapplication/CustomerSignupActivity.java
@@ -1,44 +1,69 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CustomerSignupActivity extends AppCompatActivity {
-    EditText e1,e2,e3,e4;
-    Button b1;
+    EditText mname,memail,mphone,mpassword;
+    Button mbutton;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_signup);
-        e1=findViewById(R.id.editTextEditName);
-        e2=findViewById(R.id.editTextEditPhone);
-        e3=findViewById(R.id.email);
-        e4=findViewById(R.id.editTextEditPassword);
-        b1=findViewById(R.id.buttonregister);
+        mname=findViewById(R.id.editTextEditName1);
+        mphone=findViewById(R.id.editTextEditPhone1);
+        memail=findViewById(R.id.email1);
+        mpassword=findViewById(R.id.editTextEditPassword1);
+        mbutton=findViewById(R.id.buttonregister11);
 
-        String name=e1.getText().toString();
-        String phoneNo=e2.getText().toString().trim();
-        String email=e3.getText().toString();
-        String password=e4.getText().toString();
-        b1.setOnClickListener(new View.OnClickListener() {
+        fAuth=FirebaseAuth.getInstance();
+
+        mbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    Intent intent=new Intent(getApplicationContext(),CustomerVerifyPhoneNumber.class);
-                    intent.putExtra("phoneNo",phoneNo);
-                    startActivity(intent);
+                String email=memail.getText().toString().trim();
+                String password=mpassword.getText().toString().trim();
+                if(TextUtils.isEmpty(email)){
+                    memail.setError("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mpassword.setError("Password is Required");
+                    return;
+                }
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(CustomerSignupActivity.this,"User Created",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),ShopListActivity.class));
+                        }
+                        else {
+                            Toast.makeText(CustomerSignupActivity.this,"Error " +task.getException(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
+
     }
-    public void Signup(View view)
-    {
-        Intent intent = new Intent(this, ShopListActivity.class);
-        startActivity(intent);
-        this.finish();
-    }
+
 }
